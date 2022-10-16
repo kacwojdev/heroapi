79c8b04b3afbd9147b9cce2b63da462a10c30be1
kacwoj <wojnowski.kacper@gmail.com>
2022-10-16 13:15:28 +0200

    Updates

---
+++ precache-manifest.53f877c76ee584124986a5088c88e3e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d8bd9bb81ae5204d6db",
+    "url": "/heroapi/static/css/main.7411145a.chunk.css"
+  },
+  {
+    "revision": "2d8bd9bb81ae5204d6db",
+    "url": "/heroapi/static/js/main.2d8bd9bb.chunk.js"
+  },
+  {
+    "revision": "d7736f3071f31b305530",
+    "url": "/heroapi/static/js/1.d7736f30.chunk.js"
+  },
+  {
+    "revision": "06e0735578723033c1db",
+    "url": "/heroapi/static/js/runtime~main.06e07355.js"
+  },
+  {
+    "revision": "80e4ce7501d8c9a907fedd150959cef9",
+    "url": "/heroapi/index.html"
+  }
+];
